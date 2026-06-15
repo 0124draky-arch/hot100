a459f267663632146313177049c0028af0f72bdc
draky <draky@foxmail.com>
2026-06-15 22:09:24 +0800

    1，128复习

--- 哈希表与数组/lc1.js
+++ 哈希表与数组/lc1.js
@@ -3,12 +3,14 @@
  * @param {number} target
  * @return {number[]}
  */
+//值是键，下标是键对应的值
 var twoSum = function(nums, target) {
     const map = new Map();
     for(let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if(map.has(complement)) 
-            return [map.get(complement), i];
+        const need = target - nums[i];
+        if(map.has(need)) {
+            return [map.get(need), i];
+        }
         map.set(nums[i], i);
     }
 };

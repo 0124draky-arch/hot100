047eb8bc579826ed2a68a217e37f937c6eb062ef
draky <draky@foxmail.com>
2026-06-24 14:32:28 +0800

    11

---
+++ 双指针/lc11.js
@@ -0,0 +1,21 @@
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let max = 0
+
+    while(left < right) {
+        const width = right - left;
+        const h = Math.min(height[left], height[right]);
+        const area = width * h;
+
+        max = Math.max(max, area);
+
+        if(height[left] >= height[right]) {
+            right--;
+        } else {
+            left++
+        }
+    }
+
+    return max;
+};

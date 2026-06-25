8f80ce73fc6fe1ac50ed5c09e53ec7f2df72fadf
draky <draky@foxmail.com>
2026-06-25 13:54:37 +0800

    53

---
+++ 双指针/lc53.js
@@ -0,0 +1,11 @@
+var maxSubArray = function(nums) {
+    let currentMax = nums[0];
+    let maxSum = nums[0];
+
+    for(let i = 1; i < nums.length; i++) {
+        currentMax = Math.max(nums[i], nums[i] + currentMax);
+        maxSum = Math.max(currentMax, maxSum);
+    }
+
+    return maxSum;
+};
